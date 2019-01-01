645bc3dd3661640756a9d0804c3e2f574b9f30cc
Nikolay Sohryakov <Nikolay.Sohryakov@gmail.com>
2019-01-01 17:48:03 +0300

    Initial committ

---
+++ SwiftyPecker/SwiftyPecker.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyPecker.h
+//  SwiftyPecker
+//
+//  Created by Nikolay Sohryakov on 01/01/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyPecker.
+FOUNDATION_EXPORT double SwiftyPeckerVersionNumber;
+
+//! Project version string for SwiftyPecker.
+FOUNDATION_EXPORT const unsigned char SwiftyPeckerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPecker/PublicHeader.h>
+
+
