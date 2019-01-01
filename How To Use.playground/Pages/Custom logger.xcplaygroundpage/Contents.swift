bb478542487998d19300db49b672db4dc1241b9f
Nikolay Sohryakov <Nikolay.Sohryakov@gmail.com>
2019-01-01 19:21:11 +0300

    Fixed message typo

--- How To Use.playground/Pages/Custom logger.xcplaygroundpage/Contents.swift
+++ How To Use.playground/Pages/Custom logger.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@ let category = "Chat"
 let log = SwiftyOSLog(subsystem: subsystem, category: category)
 
 log.info("Info message")
-log.debug("Debug messadge")
+log.debug("Debug message")
 log.error("Error message")
 log.fault("Fault message")
 //: [Next](@next)
--- How To Use.playground/Pages/Default logger.xcplaygroundpage/Contents.swift
+++ How To Use.playground/Pages/Default logger.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ import SwiftyOSLog
 let log = SwiftyOSLog.default
 
 log.info("Info message")
-log.debug("Debug messadge")
+log.debug("Debug message")
 log.error("Error message")
 log.fault("Fault message")
 //: [Next](@next)
