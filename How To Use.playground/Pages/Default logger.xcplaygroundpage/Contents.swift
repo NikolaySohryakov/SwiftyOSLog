435c92a907d6536028fc7514c889dca2af2f4b1c
Nikolay Sohryakov <Nikolay.Sohryakov@gmail.com>
2019-01-03 17:54:03 +0300

    Added support for a custom format and builtin value type decoders

--- How To Use.playground/Pages/Default logger.xcplaygroundpage/Contents.swift
+++ How To Use.playground/Pages/Default logger.xcplaygroundpage/Contents.swift
@@ -5,9 +5,10 @@ import SwiftyOSLog
 
 let log = SwiftyOSLog.default
 
-log.default("Default message")
-log.info("Info message")
+log.default("%{private}@ - %{public}@", "Private Part", "Public Part")
+log.info("This Info message logged at %{time_t}d", Date().timeIntervalSince1970)
 log.debug("Debug message")
 log.error("Error message")
 log.fault("Fault message")
+
 //: [Next](@next)
--- SwiftyOSLog/SwiftyOSLog.swift
+++ SwiftyOSLog/SwiftyOSLog.swift
@@ -10,8 +10,8 @@ import os.log
 
 public class SwiftyOSLog {
     private var log: OSLog
-    private var subsystem: Subsystem?
-    private var category: Category?
+    public private(set) var subsystem: Subsystem?
+    public private(set) var category: Category?
     
     //region Init
     
@@ -33,61 +33,88 @@ public class SwiftyOSLog {
     //endregion
     
     //region Default
-    
-    public func `default`(_ message: String) {
-        log(message, type: .info)
-    }
      
-    public func `default`(_ message: StaticString) {
-        log(message, type: .default)
+    public func `default`(_ format: StaticString, _ args: CVarArg...) {
+        log(format, type: .default, args)
     }
     
     //endregion
     
     //region Info
     
-    public func info(_ message: String) {
-        log(message, type: .info)
-    }
-    
-    public func info(_ message: StaticString) {
-        log(message, type: .info)
+    public func info(_ format: StaticString, _ args: CVarArg...) {
+        log(format, type: .info, args)
     }
     
     //endregion
     
     //region Debug
     
-    public func debug(_ message: String) {
-        log(message, type: .debug)
-    }
-    
-    public func debug(_ message: StaticString) {
-        log(message, type: .debug)
+    public func debug(_ format: StaticString, _ args: CVarArg...) {
+        log(format, type: .debug, args)
     }
     
     //endregion
     
     //region Error
     
-    public func error(_ message: String) {
-        log(message, type: .error)
-    }
-    
-    public func error(_ message: StaticString) {
-        log(message, type: .error)
+    public func error(_ format: StaticString, _ args: CVarArg...) {
+        log(format, type: .error, args)
     }
     
     //endregion
     
     //region Fault
     
-    public func fault(_ message: String) {
-        log(message, type: .fault)
+    public func fault(_ format: StaticString, _ args: CVarArg...) {
+        log(format, type: .fault, args)
     }
     
-    public func fault(_ message: StaticString) {
-        log(message, type: .fault)
+    //endregion
+    
+    //region Log
+    
+    public func log(_ format: StaticString, type: OSLogType, _ passedArgs: CVarArg...) {
+        //At some point we have passedArgs as an array of array. Need to flat it checking types,
+        //that's why functional approach is not suitable here.
+        let args: [CVarArg]
+        if passedArgs.count == 1, let _args = passedArgs.first as? [CVarArg] {
+            args = _args
+        }
+        else {
+            args = passedArgs
+        }
+        
+        // When passing `args` directly to os_log, it does crash with an unknown error.
+        // This simple and silly solution makes it possible to pass up to 10 arguments to os_log.
+        switch args.count {
+        case 0:
+            os_log(format, log: log, type: type)
+        case 1:
+            print("Args[0] = \(args[0])")
+            print("Format: \(format)")
+            os_log(format, log: log, type: type, args[0])
+        case 2:
+            os_log(format, log: log, type: type, args[0], args[1])
+        case 3:
+            os_log(format, log: log, type: type, args[0], args[1], args[2])
+        case 4:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3])
+        case 5:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4])
+        case 6:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4], args[5])
+        case 7:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+        case 8:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7])
+        case 9:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8])
+        case 10:
+            os_log(format, log: log, type: type, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9])
+        default:
+            os_log(format, log: log, type: type, args)
+        }
     }
     
     //endregion
@@ -97,15 +124,4 @@ extension SwiftyOSLog: Equatable {
     public static func == (lhs: SwiftyOSLog, rhs: SwiftyOSLog) -> Bool {
         return lhs.subsystem == rhs.subsystem && lhs.category == rhs.category
     }
-}
-
-private extension SwiftyOSLog {
-    func log(_ message: String, type: OSLogType) {
-        os_log("%{public}@", log: log, type: type, message)
-    }
-    
-    func log(_ message: StaticString, type: OSLogType) {
-        os_log(message, log: log, type: type)
-    }
-
 }
